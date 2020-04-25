fa99ef3784fc293dc6438705ab9326bb89969256
BenjaminMassey <benjamin.w.massey@gmail.com>
2020-04-25 14:58:17 -0700

    Made GlobalMethods and have and use things in it

---
+++ Assets/Scripts/GlobalMethods.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GlobalMethods
+{
+    public static Vector3 getVectorInFront(Transform t, float amount)
+    {
+        Vector3 origPos = t.position;
+        Quaternion origRot = t.rotation;
+        t.Translate(Vector3.forward);
+        Vector3 result = t.position;
+        t.position = origPos;
+        t.rotation = origRot;
+
+        return result;
+    }
+
+    public static GameObject TestHit(Transform t, float distance, float offset = 0.0f)
+    {
+        // Sees if object is within distance units of player's looking, returns if so (otherwise null)
+
+        RaycastHit rchit;
+        bool hit = Physics.Raycast(getVectorInFront(t, offset),
+                                   t.TransformDirection(Vector3.forward),
+                                   out rchit,
+                                   distance);
+        if (!hit)
+        {
+            return null;
+        }
+        else
+        {
+            return rchit.collider.gameObject;
+        }
+    }
+}
