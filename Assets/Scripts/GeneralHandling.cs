3acf2d26b84e62d0b16767b92e4cc438876bfeed
BenjaminMassey <benjamin.w.massey@gmail.com>
2020-04-29 17:12:46 -0700

    Reworked text/canvas to be better, added escape message

---
+++ Assets/Scripts/GeneralHandling.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GeneralHandling : MonoBehaviour
+{
+
+    Text t;
+    int defaultFont;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = GameObject.Find("Text").GetComponent<Text>();
+        defaultFont = t.fontSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            StartCoroutine("EscMessage");
+        }
+    }
+
+    IEnumerator EscMessage()
+    {
+        t.text = "MOUSE IS FREE";
+        t.fontSize = defaultFont + 6;
+        yield return new WaitForSeconds(10.0f);
+        t.fontSize = defaultFont - 6;
+    }
+}
