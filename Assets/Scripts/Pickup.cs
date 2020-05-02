a88ee4b6ba5dfa3493f3cbd1df7276c6bcaace4e
emstrubl <63114742+emstrubl@users.noreply.github.com>
2020-05-02 01:21:20 -0700

    Pickupable boxes now highlight when able to be picked up

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -9,6 +9,13 @@ public class Pickup : MonoBehaviour
 
     // Attached to player
 
+    public Material highlightMat;
+
+    private Material origMat;
+    private bool highlighting; // whether an object is highlighted
+    private GameObject highlightedObj; // what object is being highlighted (null if nothing)
+    // objects do not highlight if time is reversing or if the player is holding something
+
     private bool holding; // whether player is holding something
     private GameObject heldObj; // what player is holding (null if nothing)
     private ReverseTime rt;
@@ -17,7 +24,9 @@ public class Pickup : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        highlighting = false;
         holding = false;
+        origMat = null;
         rt = GameObject.Find("Player").GetComponent<ReverseTime>();
         forward = true;
     }
@@ -26,33 +35,71 @@ public class Pickup : MonoBehaviour
     void Update()
     {
         forward = rt.GetTimeForward();
-        if (Input.GetKeyDown(KeyCode.Mouse0) && forward)
+        if (forward)
         {
             if (!holding)
             {
                 //GameObject obj = TestHit(); // looks 3 units straight from where looking
                 GameObject obj = GlobalMethods.TestHit(transform, 3.0f, 0.25f);
-                if (obj != null)
+                if (obj != null && !highlighting)
                 {
-                    Debug.Log("Got " + obj.name);
                     if (obj.name == "Box") // TODO: implement actual tag feature of pickup-ables
                     {
-                        heldObj = obj;
+                        Highlight(obj);
+                    }
+                }
+                else if (obj == null && highlighting)
+                {
+                    Unhighlight();
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+
+                if (!holding)
+                {
+                    if (highlighting)
+                    {
+                        Debug.Log("Got " + highlightedObj.name);
+                        heldObj = highlightedObj;
+                        Unhighlight();
                         heldObj.gameObject.GetComponent<Rigidbody>().useGravity = false;
                         //heldObj.gameObject.GetComponent<Rigidbody>().isKinematic = true;
                         // want object to be able to be moved around freely, so no physics stuff
+
                     }
                 }
+
+                else
+                {
+                    heldObj.gameObject.GetComponent<Rigidbody>().useGravity = true;
+                    //heldObj.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+                    // object can have physics again, now that not in hand
+                    heldObj = null;
+                }
             }
-            else
-            {
-                heldObj.gameObject.GetComponent<Rigidbody>().useGravity = true;
-                //heldObj.gameObject.GetComponent<Rigidbody>().isKinematic = false;
-                // object can have physics again, now that not in hand
-                heldObj = null;
-            }
+            HandleHolding();
         }
-        HandleHolding();
+    }
+
+    void Highlight(GameObject obj)
+    {
+        Debug.Log("highlighted " + obj.name);
+        origMat = obj.GetComponent<Renderer>().material;
+        obj.GetComponent<Renderer>().material = highlightMat;
+        highlighting = true;
+        highlightedObj = obj;
+        //highlightedObj.GetComponent<Renderer>().material.SetColor("__EmissionColor", Color.red);
+
+    }
+
+    void Unhighlight()
+    {
+        Debug.Log("unhighlighted " + highlightedObj.name);
+        highlightedObj.GetComponent<Renderer>().material = origMat;
+        origMat = null;
+        highlighting = false;
+        highlightedObj = null;
     }
 
     void HandleHolding()
