ba6ced6ff12bae9dd7102dfa65956c6e0c3f907f
BenjaminMassey <benjamin.w.massey@gmail.com>
2020-05-11 22:09:33 -0700

    Changed push box to have outlined area

--- Assets/Scripts/Highlighter.cs
+++ Assets/Scripts/Highlighter.cs
@@ -7,8 +7,8 @@ public class Highlighter : MonoBehaviour
 
     // Attached to player camera
 
-    public Material highlightMat;
-    private Material origMat;
+    //public Material highlightMat;
+    //private Material origMat;
 
     private GameObject highlightedObj; // what object is being highlighted (null if nothing)
     // objects do not highlight if time is reversing or if the player is holding something
@@ -17,25 +17,30 @@ public class Highlighter : MonoBehaviour
     private bool forward;
     private Pickup pu;
     private bool holding;
+    private Color highlightColor;
 
     // Start is called before the first frame update
     void Start()
     {
         highlightedObj = null;
-        origMat = null;
+        //origMat = null;
         rt = GameObject.Find("Player").GetComponent<ReverseTime>();
         pu = GameObject.Find("FirstPersonCharacter").GetComponent<Pickup>();
         forward = true;
         holding = false;
+        highlightColor = new Color(0.25f, 0.25f, 0.25f, 0.0f);
+        InvokeRepeating("CheckHighlight", 0.0f, 1.0f / 15.0f);
     }
 
-    // Update is called once per frame
-    void Update()
+    void CheckHighlight()
     {
         forward = rt.GetTimeForward();
         if (forward)
         {
-            holding = pu.GetHolding();
+            if (pu != null)
+            {
+                holding = pu.IsHolding();
+            }
             if (!holding)
             {
                 //GameObject obj = TestHit(); // looks 3 units straight from where looking
@@ -52,7 +57,7 @@ public class Highlighter : MonoBehaviour
                 {
                     Debug.Log("hi");
                     if (GlobalMethods.ObjectInArray(obj, GlobalMethods.GetReversables())) // TODO: add functionality for nonpickupables, and for frozen objects
-                                                            // (need another material for all possible cases)
+                                                                                          // (need another material for all possible cases)
                     {
                         Highlight(obj);
                     }
@@ -70,8 +75,10 @@ public class Highlighter : MonoBehaviour
     void Highlight(GameObject obj)
     {
         Debug.Log("highlighted " + obj.name);
-        origMat = obj.GetComponent<Renderer>().material;
-        obj.GetComponent<Renderer>().material = highlightMat;
+        //origMat = obj.GetComponent<Renderer>().material;
+        //obj.GetComponent<Renderer>().material = highlightMat;
+        Renderer r_obj = obj.GetComponent<Renderer>();
+        r_obj.material.SetColor("_Color", r_obj.material.color + highlightColor);
         highlightedObj = obj;
         //highlightedObj.GetComponent<Renderer>().material.SetColor("__EmissionColor", Color.red);
 
@@ -80,8 +87,10 @@ public class Highlighter : MonoBehaviour
     void Unhighlight()
     {
         Debug.Log("unhighlighted " + highlightedObj.name);
-        highlightedObj.GetComponent<Renderer>().material = origMat;
-        origMat = null;
+        //highlightedObj.GetComponent<Renderer>().material = origMat;
+        //origMat = null;
+        Renderer r_obj = highlightedObj.GetComponent<Renderer>();
+        r_obj.material.SetColor("_Color", r_obj.material.color - highlightColor);
         highlightedObj = null;
     }
 
