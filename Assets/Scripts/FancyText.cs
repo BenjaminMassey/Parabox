bbfbf9694eb8df6cf7fa71dbbf328b454ba2e1fd
BenjaminMassey <benjamin.w.massey@gmail.com>
2020-05-15 01:12:28 -0700

    Setup a way to have messages pop up: FancyText. Now Canvas is a prefab (and should be used in every scene)

---
+++ Assets/Scripts/FancyText.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class FancyText : MonoBehaviour
+{
+    // To be placed on Canvas named "Canvas"
+    // To be used by other methods via FancyTextHandler (below)
+
+    public Image bg;
+    public Text text;
+
+    private string message;
+    private float hangtime; // in seconds
+    private bool inUse;
+
+    private void Start()
+    {
+        bg.enabled = false;
+        text.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Message("Left Click to pickup boxes", 3f);
+            //Message("this is going to be a very very long message on the first line\nthen not", 3f);
+            //Message("very\nvery\nvery\nvery\nvery\ntall", 3f);
+        }
+    }
+
+    public bool Message(string s, float time, bool force = false) {
+        if (force)
+        {
+            StopAllCoroutines();
+            inUse = false;
+        }
+        if (!inUse)
+        {
+            message = s;
+            hangtime = time;
+            StartCoroutine("PostMessage");
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private IEnumerator PostMessage()
+    {
+        inUse = true;
+
+        Color bgc = bg.color;
+        bgc.a = 0f;
+        bg.color = bgc;
+
+        Color tc = text.color;
+        tc.a = 0f;
+        text.color = tc;
+
+        bg.enabled = true;
+        text.enabled = true;
+
+        text.text = message;
+
+        string[] lines = message.Split('\n');
+        int numLines = lines.Length;
+        int maxLength = 0;
+        foreach (string line in lines)
+        {
+            maxLength = Mathf.Max(maxLength, line.Length);
+        }
+        RectTransform rt = bg.GetComponent<RectTransform>();
+        Vector2 size = new Vector3(30.0f, 110.0f); // 40px per char, 125px per line
+        size.x *= maxLength;
+        size.y *= numLines;
+        rt.sizeDelta = size;
+
+        int framesToFade = 45;
+        float val;
+        for (int i = 1; i <= framesToFade; i++) // fade in
+        {
+            val = (i * (255f / framesToFade)) / 255f;
+            //Debug.Log("ALPHA VALUE: " + val);
+            bgc.a = val;
+            bg.color = bgc;
+            tc.a = val;
+            text.color = tc;
+            yield return new WaitForFixedUpdate();
+        }
+
+        yield return new WaitForSeconds(hangtime);
+
+        for (int i = framesToFade; i > 0; i--) // fade out
+        {
+            val = (i * (255f / framesToFade)) / 255f;
+            //Debug.Log("ALPHA VALUE: " + val);
+            bgc.a = val;
+            bg.color = bgc;
+            tc.a = val;
+            text.color = tc;
+            yield return new WaitForFixedUpdate();
+        }
+
+        text.text = "";
+
+        bg.enabled = false;
+        text.enabled = false;
+
+        inUse = false;
+    }
+}
+
+public static class FancyTextHandler {
+    public static bool Message(string msg, float time, bool force = false)
+    {
+        return GameObject.Find("Canvas").GetComponent<FancyText>().Message(msg, time, force);
+    }
+}
