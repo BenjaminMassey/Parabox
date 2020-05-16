9af050511b302795bbb121ac86a9dc2eb9919202
BenjaminMassey <benjamin.w.massey@gmail.com>
2020-05-15 21:31:03 -0700

    Made it so mouse unlocks when escape is pressed

--- Assets/Scripts/FPSCameraController.cs
+++ Assets/Scripts/FPSCameraController.cs
@@ -12,20 +12,40 @@ public class FPSCameraController : MonoBehaviour
     private float mouseY;
     private float xRotation;
 
+    private bool active;
+
     private void Awake()
     {
+        active = true;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void Update()
     {
-        mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity;
-        mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+        if (active)
+        {
+            mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity;
+            mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        player.transform.rotation *= Quaternion.Euler(Vector3.up * mouseX);
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            player.transform.rotation *= Quaternion.Euler(Vector3.up * mouseX);
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                active = false;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0) && IsMouseOverGameWindow)
+            {
+                active = true;
+            }
+        }
     }
+
+    // http://answers.unity.com/answers/1681937/view.html
+    bool IsMouseOverGameWindow { get { return !(0 > Input.mousePosition.x || 0 > Input.mousePosition.y || Screen.width < Input.mousePosition.x || Screen.height < Input.mousePosition.y); } }
 }
