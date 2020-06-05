d92ea5197d62d73b77d0860373329f2b4a7605bc
Justin Ryals <emstrubl2@gmail.com>
2020-06-05 00:11:57 -0700

    changed door handler to on awake instead of on start

--- Assets/Scripts/DoorHandler.cs
+++ Assets/Scripts/DoorHandler.cs
@@ -6,7 +6,7 @@ public class DoorHandler : MonoBehaviour
 {
     public int numPressed;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         numPressed = 0;
     }
